0fb2c474f1dd5fe96692cddb74adedaf4e490e5e
wilinz <weiizan71@gmail.com>
2023-05-05 01:53:27 +0800

    update

---
+++ lib/ui/page/about/about.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'about.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class LinkButton extends StatelessWidget {
+  const LinkButton(
+    this.url,
+    this.label, {
+    Key? key,
+  }) : super(key: key);
+
+  final String url;
+
+  final String label;
+
+  @override
+  Widget build(BuildContext _context) => linkButton(
+        _context,
+        url,
+        label,
+      );
+}
